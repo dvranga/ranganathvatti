7d88fb8c59c88cfa03a9d46dddd77bdfe224c444
Ranganath Vatti <62434972+dvranga@users.noreply.github.com>
2025-08-04 10:39:07 +0530

    Update Hero.jsx
    updated years of experience

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,12 +3,19 @@ import styles from './Hero.module.css';
 import { getImageUrl } from './../../utils';
 
 const Hero = () => {
+function getYearsOfExperience(startYear = 2021, startMonth = 0) {
+  const startDate = new Date(startYear, startMonth); // January is month 0
+  const now = new Date();
+  const diffInMs = now - startDate;
+  const diffInYears = diffInMs / (1000 * 60 * 60 * 24 * 365.25);
+  return Math.floor(diffInYears);
+}
     return (
         <section className={styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I am Ranganath Vatti</h1>
                 <p className={styles.description}>
-                    I am a senior software engineer, with 3 years of experience in frontend technology
+                    I am a senior software engineer, with {getYearsOfExperience()} years of experience in frontend technology
                 </p>
                 <a className={styles.contactBtn} href='mailto:work.ranganath@gmail.com'>Contact me</a>
             </div>
